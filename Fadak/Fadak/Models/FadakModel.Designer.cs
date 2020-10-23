b8f5742bb0019b9576b24d0baa7361bbdd7be7f3
zavoshsoftware <70507208+zavoshsoftware@users.noreply.github.com>
2020-10-23 18:36:39 +0330

    bug fix

--- Fadak/Fadak/Models/FadakModel.Designer.cs
+++ Fadak/Fadak/Models/FadakModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\asus\Documents\Visual Studio 2015\Projects\Fadak\Fadak\Models\FadakModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Hossein\Documents\GitHub\FadakFelez\Fadak\Fadak\Models\FadakModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
